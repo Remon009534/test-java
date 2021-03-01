0349a98b2787ebd5a127a6f3fb4725aa3cdd9b60
Remon009534 <70149212+Remon009534@users.noreply.github.com>
2021-03-01 13:53:12 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Mobile m1 = new Mobile("Remon", "012346969696", "Ios", 100, false);
+
+        m1.showinfo();
+
+    }
+}
